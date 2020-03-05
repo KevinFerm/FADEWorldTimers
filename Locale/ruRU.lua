0506541ff1cf0b726a4b879583852b1a072b251d
Kevin Ferm <kevinferm@gmail.com>
2020-03-05 17:55:53 +0100

    Added ruRU locale

---
+++ Locale/ruRU.lua
@@ -0,0 +1,8 @@
+-- ruRU.lua
+local L = LibStub("AceLocale-3.0"):NewLocale("FADEWT", "ruRU")
+if not L then return end
+
+L["Overlord Runthak"] = "Властитель Рунтак"
+L["Major Mattingly"] = "Майор Маттингли"
+L["High Overlord Saurfang"] = "Верховный воевода Саурфанг"
+L["Field Marshal Afrasiabi"] = "Фельдмаршал Афрасиаби"
