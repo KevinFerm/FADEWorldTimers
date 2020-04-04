95d12d7f86ac2d2b74aa73526cf529c6091f5cbc
Kevin Ferm <kevinferm@gmail.com>
2020-04-04 15:04:28 +0200

    Added french locale

---
+++ Locale/frFR.lua
@@ -0,0 +1,7 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("FADEWT", "frFR")
+if not L then return end
+
+L["Overlord Runthak"] = "Seigneur Runthak"
+L["Major Mattingly"] = "Major Mattingly"
+L["High Overlord Saurfang"] = "Haut seigneur Saurfang"
+L["Field Marshal Afrasiabi"] = "Grand maréchal Afrasiabi"
